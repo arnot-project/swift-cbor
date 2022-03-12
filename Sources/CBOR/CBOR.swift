ef8cd1d465d4d0475b6cd485d3de8f2243693c5a
FRAGKISKOS KATSIMPAS <fragkiskos.katsimpas@gmail.com>
2022-03-12 20:53:20 +0000

    First test

--- Sources/CBOR/CBOR.swift
+++ Sources/CBOR/CBOR.swift
@@ -1,4 +1,18 @@
 public struct CBOREncoder {
     
     public init() {}
+    
+    public func encode(_ entity: Transaction) -> String {
+        return entity.utxo == nil ? "80" : "81A0"
+    }
+}
+
+public struct  Transaction {
+    var utxo: Int?
+    
+    public init() {}
+    
+    public mutating func addUtxoIn() {
+        utxo = 0
+    }
 }
--- Tests/CBORTests/CBORTests.swift
+++ Tests/CBORTests/CBORTests.swift
@@ -6,10 +6,25 @@ final class CBORTests: XCTestCase {
     func testCanInitialise() {
         // given
         let sut = CBOREncoder()
+        let entity = Transaction()
 
         // when
+        let result = sut.encode(entity)
 
         // then
+        XCTAssertEqual(result, "80")
+    }
+    
+    func testUtxoIn() {
+        // given
+        let sut = CBOREncoder()
+        var entity = Transaction()
+        entity.addUtxoIn()
+
+        // when
+        let result = sut.encode(entity)
+
+        // then
+        XCTAssertEqual(result, "81A0")
     }
 }
-
